4bd5a15e6319a021c1b9899e0d3db23fb267dd0d
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2015-04-07 19:46:07 +0200

    ForecastServiceTest

--- src/test/java/com/github/wkicior/helyeah/service/ForecastServiceTest.java
+++ src/test/java/com/github/wkicior/helyeah/service/ForecastServiceTest.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -20,28 +22,34 @@ public class ForecastServiceTest {
 	@Mock
 	WebTarget wwoProxy;
 	
+	@Mock
+	WebTarget notificationService;
+	
 	
 	@InjectMocks
 	ForecastService service;
 	
 	@Before
 	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-	
+		MockitoAnnotations.initMocks(this);	
 	}
 
 	@Test
 	public void testProcessLocation() {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("latitude", Double.toString(12.0));
-		params.put("longitude", Double.toString(13.0));
+		params.put("longitude", Double.toString(13.5));
 		WebTarget resolvedWwoProxy = Mockito.mock(WebTarget.class);
 		Mockito.when(wwoProxy.resolveTemplates(params)).thenReturn(resolvedWwoProxy);
-		Builder builder = Mockito.mock(Builder.class);
+		Builder builderWwoProxy = Mockito.mock(Builder.class);
+		Builder builderNotificationService = Mockito.mock(Builder.class);
 		JsonObject wwoProxyJsonResponse = Json.createObjectBuilder().build();
-		Mockito.when(builder.get(JsonObject.class)).thenReturn(wwoProxyJsonResponse);
-		Mockito.when(resolvedWwoProxy.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
+		Mockito.when(builderWwoProxy.get(JsonObject.class)).thenReturn(wwoProxyJsonResponse);
+		Mockito.when(resolvedWwoProxy.request(MediaType.APPLICATION_JSON)).thenReturn(builderWwoProxy);
+		Mockito.when(notificationService.request(MediaType.APPLICATION_JSON)).thenReturn(builderNotificationService);
+		
 		service.processLocation(12.0, 13.5);
+		Mockito.verify(builderNotificationService).post(Mockito.any(Entity.class));
 	}
 
 }
